22b85fa8ec8b137bddd564587a7ae841eafbcdea
sarah bohadana <sarah.bohadana@gmail.com>
2024-02-18 18:51:35 +0200

    added first variables and save and show task func to the js

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const inputBox = document.querySelector("#inputBox");
+const listContainer = document.querySelector("#listContainer");
+const addButton = document.querySelector(".add");
+const spanCounter = document.querySelector(".counter");
+const allButton = document.querySelector(".all");
+const activeButton = document.querySelector(".active");
+const completedButton = document.querySelector(".completed");
+const clearButton = document.querySelector(".clear");
+let counter = localStorage.getItem("count");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+inputBox.addEventListener("keydown", function(e){
+    if(e.key === "Enter")
+      return addTask()
+});
+
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+};
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+};
+showTask();
+
